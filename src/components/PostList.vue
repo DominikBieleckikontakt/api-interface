ccf5bc146484ac00193d3c6ca340bcd673047ea8
Dominik <dominikbielecki.kontakt@gmail.com>
2025-08-02 08:10:49 +0200

    Added working pagination

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,8 +2,9 @@
   <div>
     <div v-if="posts.length === 0">Loading data...</div>
     <div v-else>
-      <h1 class="mb-16">Posts</h1>
-      <div class="grid lg:grid-cols-2 2xl:grid-cols-3 gap-16">
+      <h1 class="mb-16">Posty</h1>
+
+      <div class="grid gap-16 lg:grid-cols-2 2xl:grid-cols-3">
         <PostItem
           v-for="post in posts"
           :key="post.id"
@@ -12,17 +13,41 @@
           @delete="deletePost(post.id)"
         />
       </div>
-      <div class="mt-16 flex justify-between">
+
+      <div class="flex justify-between mt-16">
         <button
-          v-for="page in totalPages"
-          :key="page"
-          :class="[
-            'px-3 py-1 border border-gray-300 rounded-lg',
-            page === currentPage ? 'bg-blue-500 text-white' : '',
-          ]"
-          @click="changePage(page)"
+          @click="changePage(currentPage - 1)"
+          class="flex items-center px-3 py-1 border rounded"
+          :class="{
+            visible: currentPage > 1,
+            invisible: currentPage <= 1,
+          }"
         >
-          {{ page }}
+          <ArrowLeft class="w-4 h-4 mr-2" />
+          Wstecz
+        </button>
+
+        <div class="hidden sm:block">
+          Strona
+          <input
+            class="w-auto p-2 py-0 text-center border border-gray-300 rounded-lg max-w-9 outline-0"
+            :value="currentPage"
+            @change="(e) => changePage(Number(e.target.value))"
+          />
+          z
+          {{ totalPages }}
+        </div>
+
+        <button
+          @click="changePage(currentPage + 1)"
+          class="flex items-center px-3 py-1 border rounded"
+          :class="{
+            visible: currentPage < totalPages,
+            invisible: currentPage === totalPages,
+          }"
+        >
+          Dalej
+          <ArrowRight class="w-4 h-4 ml-2" />
         </button>
       </div>
     </div>
@@ -33,6 +58,7 @@
 import { onMounted, computed } from "vue";
 import { useStore } from "vuex";
 import PostItem from "./PostItem.vue";
+import { ArrowLeft, ArrowRight } from "lucide-vue-next";
 
 const store = useStore();
 
@@ -42,8 +68,8 @@ onMounted(() => {
 
 const posts = computed(() => store.getters.paginatedPosts);
 const totalPages = computed(() => store.getters.totalPages);
-const currentPage = computed(() => store.getters.currentPage);
-const getUserById = computed(() => store.getters.getUserById);
+const currentPage = computed(() => store.state.currentPage);
+const getUserById = store.getters.getUserById;
 
 const changePage = (page) => {
   store.commit("setCurrentPage", page);
